729944711a6543ef2fec58b22230c44e0b1e0424
Redev325 <s1991375@online.houstonisd.org>
2026-09-22 18:39:09 -0500

    Restore MusicBeatState UI base class

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -8,15 +8,12 @@ import flixel.util.FlxColor;
 import openfl.Lib;
 import Conductor.BPMChangeEvent;
 import flixel.FlxG;
-import flixel.FlxState;
 import flixel.addons.transition.FlxTransitionableState;
-#if !web
 import flixel.addons.ui.FlxUIState;
-#end
 import flixel.math.FlxRect;
 import flixel.util.FlxTimer;
 
-class MusicBeatState extends #if web FlxState #else FlxUIState #end
+class MusicBeatState extends FlxUIState
 {
 	private var lastBeat:Float = 0;
 	private var lastStep:Float = 0;
