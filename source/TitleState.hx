21bb3257ee561667a0d3bc36f2c36ff44f81dc9d
Redev325 <s1991375@online.houstonisd.org>
2026-09-23 08:21:42 -0500

    Stage full HTML5 game assets until title confirmation

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -170,7 +170,7 @@ class TitleState extends MusicBeatState
 
 		if(Main.watermarks) {
 			logoBl = new FlxSprite(-150, -100);
-			logoBl.frames = Paths.getSparrowAtlas('KadeEngineLogoBumpin');
+			logoBl.frames = #if web Paths.getSparrowAtlas('KadeEngineLogoBumpin', 'startup') #else Paths.getSparrowAtlas('KadeEngineLogoBumpin') #end;
 			logoBl.antialiasing = true;
 			logoBl.animation.addByPrefix('bump', 'logo bumpin', 24);
 			logoBl.animation.play('bump');
@@ -179,7 +179,7 @@ class TitleState extends MusicBeatState
 			// logoBl.color = FlxColor.BLACK;
 		} else {
 			logoBl = new FlxSprite(-150, -100);
-			logoBl.frames = Paths.getSparrowAtlas('logoBumpin');
+			logoBl.frames = #if web Paths.getSparrowAtlas('logoBumpin', 'startup') #else Paths.getSparrowAtlas('logoBumpin') #end;
 			logoBl.antialiasing = true;
 			logoBl.animation.addByPrefix('bump', 'logo bumpin', 24);
 			logoBl.animation.play('bump');
@@ -189,7 +189,7 @@ class TitleState extends MusicBeatState
 		}
 
 		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
-		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
+		gfDance.frames = #if web Paths.getSparrowAtlas('gfDanceTitle', 'startup') #else Paths.getSparrowAtlas('gfDanceTitle') #end;
 		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		gfDance.antialiasing = true;
@@ -197,7 +197,7 @@ class TitleState extends MusicBeatState
 		add(logoBl);
 
 		titleText = new FlxSprite(100, FlxG.height * 0.8);
-		titleText.frames = Paths.getSparrowAtlas('titleEnter');
+		titleText.frames = #if web Paths.getSparrowAtlas('titleEnter', 'startup') #else Paths.getSparrowAtlas('titleEnter') #end;
 		titleText.animation.addByPrefix('idle', "Press Enter to Begin", 24);
 		titleText.animation.addByPrefix('press', "ENTER PRESSED", 24);
 		titleText.antialiasing = true;
@@ -206,7 +206,7 @@ class TitleState extends MusicBeatState
 		// titleText.screenCenter(X);
 		add(titleText);
 
-		var logo:FlxSprite = new FlxSprite().loadGraphic(Paths.image('logo'));
+		var logo:FlxSprite = new FlxSprite().loadGraphic(#if web Paths.image('logo', 'startup') #else Paths.image('logo') #end);
 		logo.screenCenter();
 		logo.antialiasing = true;
 		// add(logo);
@@ -228,7 +228,7 @@ class TitleState extends MusicBeatState
 
 		credTextShit.visible = false;
 
-		ngSpr = new FlxSprite(0, FlxG.height * 0.52).loadGraphic(Paths.image('newgrounds_logo'));
+		ngSpr = new FlxSprite(0, FlxG.height * 0.52).loadGraphic(#if web Paths.image('newgrounds_logo', 'startup') #else Paths.image('newgrounds_logo') #end);
 		add(ngSpr);
 		ngSpr.visible = false;
 		ngSpr.setGraphicSize(Std.int(ngSpr.width * 0.8));
@@ -259,7 +259,7 @@ class TitleState extends MusicBeatState
 
 	function getIntroTextShit():Array<Array<String>>
 	{
-		var fullText:String = Assets.getText(Paths.txt('introText'));
+		var fullText:String = Assets.getText(#if web Paths.txt('introText', 'startup') #else Paths.txt('introText') #end);
 
 		var firstArray:Array<String> = fullText.split('\n');
 		var swagGoodArray:Array<Array<String>> = [];
@@ -312,19 +312,21 @@ class TitleState extends MusicBeatState
 
 			FlxG.camera.flash(FlxColor.WHITE, 1);
 			#if web
-			if (FlxG.sound.music == null || !FlxG.sound.music.playing)
-				FlxG.sound.playMusic(Paths.music('freakyMenu'), 0.7);
+			// The browser permits audio after this user gesture. The menu track
+			// is part of the tiny startup library, so it is immediately available.
+			FlxG.sound.playMusic(Paths.music('freakyMenu', 'startup'), 0.7);
+			FlxG.sound.play(Paths.sound('confirmMenu', 'startup'), 0.7);
 			#end
+			#if !web
 			FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+			#end
 
 			transitioning = true;
-			// FlxG.sound.music.stop();
-
 			MainMenuState.firstStart = true;
 
-			new FlxTimer().start(2, function(tmr:FlxTimer)
+			#if web
+			Assets.loadLibrary("game").onComplete(function(_)
 			{
-				#if web
 				Assets.loadLibrary("fonts").onComplete(function(_)
 				{
 					Assets.loadLibrary("shared").onComplete(function(_)
@@ -332,10 +334,16 @@ class TitleState extends MusicBeatState
 						FlxG.switchState(new SaveKeybinds());
 					});
 				});
-				#else
-				FlxG.switchState(new SaveKeybinds());
-				#end
+			}).onError(function(error)
+			{
+				trace(error);
 			});
+			#else
+			new FlxTimer().start(2, function(tmr:FlxTimer)
+			{
+				FlxG.switchState(new SaveKeybinds());
+			});
+			#end
 			// FlxG.sound.play(Paths.music('titleShoot'), 0.7);
 		}
 
