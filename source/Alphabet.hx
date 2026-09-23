e7b7c9bca680c4270e03cd632eccad0359894d12
Redev325 <s1991375@online.houstonisd.org>
2026-09-23 08:21:30 -0500

    Load alphabet from HTML5 startup library

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -276,7 +276,7 @@ class AlphaCharacter extends FlxSprite
 	public function new(x:Float, y:Float)
 	{
 		super(x, y);
-		var tex = Paths.getSparrowAtlas('alphabet');
+		var tex = #if web Paths.getSparrowAtlas('alphabet', 'startup') #else Paths.getSparrowAtlas('alphabet') #end;
 		frames = tex;
 
 		antialiasing = true;
