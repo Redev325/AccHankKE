a8c61b3bea6420e10318f9d4e4a1bccaa9277887
Redev325 <s1991375@online.houstonisd.org>
2026-09-23 10:27:51 -0500

    Load shared game assets before opening main menu

--- source/SaveKeybinds.hx
+++ source/SaveKeybinds.hx
@@ -3,22 +3,57 @@ package;
 import flixel.FlxG;
 import flixel.util.FlxColor;
 import flixel.text.FlxText;
+import openfl.Assets;
 
 class SaveKeybinds extends MusicBeatState
 {
     var holdTimer:Float = 0;
-    override function create() {
+    var loadingShared:Bool = false;
+
+    override function create()
+    {
         var warning:FlxText = new FlxText(0, 0, FlxG.width, 'Make sure to edit your keybinds. Press ENTER to continue.', 42);
         warning.setFormat(Paths.font("vcr.ttf"), 42, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
         warning.screenCenter();
         add(warning);
     }
 
-    override function update(elapsed:Float) {
+    override function update(elapsed:Float)
+    {
         super.update(elapsed);
 
-        if (FlxG.keys.justPressed.ENTER) {
-            FlxG.switchState(new MainMenuState());
+        if (FlxG.keys.justPressed.ENTER && !loadingShared)
+        {
+            loadingShared = true;
+
+            var loadingText:FlxText = new FlxText(
+                0,
+                FlxG.height - 80,
+                FlxG.width,
+                'Loading game assets...',
+                28
+            );
+            loadingText.setFormat(
+                Paths.font("vcr.ttf"),
+                28,
+                FlxColor.WHITE,
+                CENTER,
+                FlxTextBorderStyle.OUTLINE,
+                FlxColor.BLACK
+            );
+            add(loadingText);
+
+            Assets.loadLibrary("shared")
+                .onComplete(function(_)
+                {
+                    FlxG.switchState(new MainMenuState());
+                })
+                .onError(function(error)
+                {
+                    loadingShared = false;
+                    loadingText.text = 'Failed to load game assets. Check the browser console.';
+                    trace(error);
+                });
         }
     }
 }
